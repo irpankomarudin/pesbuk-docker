b6a9de6cd84a42187a57602ca2664f93e307b3cd
irpankomarudin <[irpankomarudin10@gmail.com]>
2020-10-20 16:23:18 +0700

    s

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $db_host = getenv('DB_HOST');
 $db_user = getenv('DB_USER');
 $db_pass = getenv('DB_PASS');
-$db_name = 'dbsosmed';
+$db_name = 'dbsossialmedia';
 
 try {    
     //create PDO connection 
